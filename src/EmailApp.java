a87ac604ae8b98840366174a3d7d890260de7a08
Thet Naung Soe <thetnaungsoe333@gmail.com>
2025-07-16 02:38:22 +0630

    employee modified 16/jul/2025 2:38

--- src/EmailApp.java
+++ src/EmailApp.java
@@ -1,6 +1,6 @@
 public class EmailApp {
     public static void main(String[] args){
-        Email eml = new Email("John","Smith");
+        Email eml = new Email("Alan","Walker");
         System.out.println(eml.showInfo());
     }
 
